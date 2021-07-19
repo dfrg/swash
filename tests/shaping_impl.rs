5edf48f0ce58c457c35f8f1d2901b770b84ccd2b
Shafkath Shuhan <shafkathshuhannyc@gmail.com>
2021-07-19 00:25:11 -0400

    fix

--- tests/shaping_impl.rs
+++ tests/shaping_impl.rs
@@ -11,31 +11,30 @@ pub fn shape(font: &str, font_size: usize, variations: &[(&str, f32)], input: &s
     let mut advance = 0.0;
     let mut output = Vec::new();
     let mut shaper = builder.build();
-    let glyph_count = shaper.metrics().glyph_count as usize;
     shaper.add_str(input);
     shaper.shape_with(|cluster| {
-        output.extend(cluster.glyphs.iter().map(|glyph| {
-            let old_advance = advance;
-            advance += glyph.advance as f64;
-            if old_advance == 0.0 {
-                format!(
+        cluster.glyphs.iter().for_each(|glyph| {
+            if advance == 0.0 {
+                output.push(format!(
                     "{}",
                     font.glyph_name(glyph.id)
                         .map(|x| x.to_string())
                         .unwrap_or(format!("gid{}", glyph.id))
-                )
+                ));
             } else {
-                format!(
+                output.push(format!(
                     "{}@{},{}",
                     font.glyph_name(glyph.id)
                         .map(|x| x.to_string())
                         .unwrap_or(format!("gid{}", glyph.id)),
-                    (glyph.x as f64 + f64::ceil(old_advance) ) as usize,
+                    (glyph.x + advance).round() as usize,
                     glyph.y as usize,
-                )
+                ));
             }
-        }));
+            advance += glyph.advance;
+        });
     });
+
     if bidi_info.has_rtl() {
         output.reverse();
     }
